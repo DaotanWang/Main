5114deea4209a2076c5e6ff05a18e39035f549d1
Douglas Wang <132033521+DaotanWang@users.noreply.github.com>
2024-10-29 23:42:30 +0800

    Update off_job_cigarette2.py

--- Excel/off_job_cigarette2.py
+++ Excel/off_job_cigarette2.py
@@ -23,11 +23,11 @@ for category in categories:
         print(f'Product Category: {category} has NaN values. Skipping this category.')
         continue
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.265, random_state=42)
 
     # 基础模型
-    xgb = XGBRegressor(n_estimators=200, max_depth=4, learning_rate=0.1, random_state=42)
-    rf = RandomForestRegressor(n_estimators=200, max_depth=5, random_state=42)
+    xgb = XGBRegressor(n_estimators=400, max_depth=5, learning_rate=0.01, random_state=42)
+    rf = RandomForestRegressor(n_estimators=400, max_depth=5, random_state=42)
 
     # 训练基础模型
     xgb.fit(X_train, y_train)
